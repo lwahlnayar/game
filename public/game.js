5ea29f1654d2d4d175d410833b62878aa7c38a0c
Rodrigo Wahl <lwahlnayar@gmail.com>
2018-09-25 19:31:15 +0200

    SETTIMEOUT HELL

--- public/game.js
+++ public/game.js
@@ -18,6 +18,10 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var borderLimitTop = -100;
+var borderLimitLeft = -100;
+var borderLimitRight = game.config.width + 100;
+var borderLimitBot = game.config.height + 100;
 
 function preload() {
     this.load.image("sky", "assets/sky2.jpg");
@@ -64,19 +68,10 @@ function create() {
             if (id === self.socket.id) {
                 setTimeout(function() {
                     addPlayer(self, players[id]);
-                }, 600);
+                }, 100);
             }
         });
-        console.log(players);
     });
-    // this.socket.on("newPlayer", function(newPlayer) {
-    //     Object.keys(players).forEach(function(id) {
-    //         if (players[id].playerId === self.socket.id) {
-    //             addPlayer(self, players[id]);
-    //         }
-    //     });
-    //     console.log(newPlayer);
-    // });
 
     //SCORE TEXTS
     scoreTextP1 = this.add.text(40, 16, "P1: 5", {
@@ -95,11 +90,8 @@ function create() {
         fontSize: "22px",
         fill: "#000"
     });
-    // this.physics.add.collider(player1, platforms);
-    console.log("PLATFORMS", platforms);
 
-    // player1.setCollideWorldBounds(false);
-
+    //Animations
     this.anims.create({
         key: "neutralRight",
         frames: [{ key: "player1", frame: 0 }],
@@ -225,25 +217,6 @@ function create() {
         frameRate: 10,
         repeat: -1
     });
-
-    //ADD PLAYERS
-    function addPlayer(self, playerInfo) {
-        curPlayer = "player" + playerInfo.playerNo;
-        self.curPlayer = players
-            .create(playerInfo.x, playerInfo.y, curPlayer)
-            .setScale(0.5, 0.5)
-            .setData({
-                alive: true,
-                lives: 5,
-                jump: 0,
-                movedLeft: false,
-                movedRight: false
-            });
-        // self.physics.add.collider(players, platforms);
-        self.curPlayer.body.setGravityY(300);
-        self.curPlayer.setBounce(0.2);
-    }
-
     // DOUBLE JUMP
     function jumpListener(player) {
         if (player.body.touching.down) {
@@ -270,13 +243,13 @@ function create() {
             this
         );
     } //CLOSE DOUBLE JUMP
-    jumpListener(player1);
-} //CLOSES CREATE FUNCTION
 
-var borderLimitTop = -100;
-var borderLimitLeft = -100;
-var borderLimitRight = game.config.width + 100;
-var borderLimitBot = game.config.height + 100;
+    setTimeout(function() {
+        if (player1) {
+            jumpListener(player1);
+        }
+    }, 200);
+} //CLOSES CREATE FUNCTION
 
 ///////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// U P D A T E ///////////////////////////////////////
@@ -285,76 +258,99 @@ var borderLimitBot = game.config.height + 100;
 function update() {
     cursors = this.input.keyboard.createCursorKeys();
 
-    //CHARACTER MOVEMENTS
+    setTimeout(function() {
+        if (player1) {
+            characterMove(player1);
+            deathCheck(player1);
+        }
+    }, 200);
+}
 
-    function characterMove(player) {
-        if (cursors.left.isDown) {
-            if (player.body.touching.down) {
-                player.setVelocityX(-200);
-                player.anims.play("leftRun", true);
-            } else {
-                player.setVelocityX(-200);
-                player.anims.play("leftJump", true);
-            }
-            player.setData({ movedRight: false });
-            player.setData({ movedLeft: true });
-        } else if (cursors.right.isDown) {
-            if (player.body.touching.down) {
-                player.setVelocityX(200);
-                player.anims.play("rightRun", true);
-            } else {
-                player.setVelocityX(200);
-                player.anims.play("rightJump", true);
-            }
-            player.setData({ movedLeft: false });
-            player.setData({ movedRight: true });
+///////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// O T H E R S ///////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+//CHARACTER MOVEMENTS
+function characterMove(player) {
+    if (cursors.left.isDown) {
+        if (player.body.touching.down) {
+            player.setVelocityX(-200);
+            player.anims.play("leftRun", true);
         } else {
-            player.setVelocityX(0);
-            if (player.data.list.movedRight) {
-                if (!player.body.touching.down) {
-                    player.anims.play("rightJumpB");
-                } else {
-                    player.anims.play("neutralRight");
-                }
+            player.setVelocityX(-200);
+            player.anims.play("leftJump", true);
+        }
+        player.setData({ movedRight: false });
+        player.setData({ movedLeft: true });
+    } else if (cursors.right.isDown) {
+        if (player.body.touching.down) {
+            player.setVelocityX(200);
+            player.anims.play("rightRun", true);
+        } else {
+            player.setVelocityX(200);
+            player.anims.play("rightJump", true);
+        }
+        player.setData({ movedLeft: false });
+        player.setData({ movedRight: true });
+    } else {
+        player.setVelocityX(0);
+        if (player.data.list.movedRight) {
+            if (!player.body.touching.down) {
+                player.anims.play("rightJumpB");
             } else {
-                if (!player.body.touching.down) {
-                    player.anims.play("leftJumpB");
-                } else {
-                    player.anims.play("neutralLeft");
-                }
+                player.anims.play("neutralRight");
+            }
+        } else {
+            if (!player.body.touching.down) {
+                player.anims.play("leftJumpB");
+            } else {
+                player.anims.play("neutralLeft");
             }
         }
     }
-    characterMove(player1);
-    //CLOSE CHARACTER MOVEMENTS
+}
+function resetPlayerPosition(player) {
+    player.x = 100; //randomize later
+    player.y = 100; //randomize later
+}
 
-    function resetPlayerPosition(player) {
-        player.x = 100; //randomize later
-        player.y = 100; //randomize later
+function deathCheck(player) {
+    var lives = player.data.list.lives;
+    if (
+        player.x > borderLimitRight ||
+        player.x < borderLimitLeft ||
+        player.y < borderLimitTop ||
+        player.y > borderLimitBot
+    ) {
+        //if breaks limits, sets player alive status to false
+        player.setData("alive", false);
+        player.setVelocity(0, 10);
+        player.setGravityY(10);
+        resetPlayerPosition(player);
+        //show death animation here
     }
+    if (!player.data.list.alive) {
+        lives--;
+        console.log("player dies");
+        player.setData({ alive: true, lives: lives });
+        // livesP1--;
+        scoreTextP1.setText("P1: " + player.data.list.lives);
+    }
+}
 
-    function deathCheck(player) {
-        var lives = player.data.list.lives;
-        if (
-            player.x > borderLimitRight ||
-            player.x < borderLimitLeft ||
-            player.y < borderLimitTop ||
-            player.y > borderLimitBot
-        ) {
-            //if breaks limits, sets player alive status to false
-            player.setData("alive", false);
-            player.setVelocity(0, 10);
-            player.setGravityY(10);
-            resetPlayerPosition(player);
-            //show death animation here
-        }
-        if (!player.data.list.alive) {
-            lives--;
-            console.log("player dies");
-            player.setData({ alive: true, lives: lives });
-            // livesP1--;
-            scoreTextP1.setText("P1: " + player.data.list.lives);
-        }
-    }
-    deathCheck(player1);
+//ADD PLAYERS
+function addPlayer(self, playerInfo) {
+    var curPlayer = "player" + playerInfo.playerNo;
+    window[curPlayer] = players
+        .create(playerInfo.x, playerInfo.y, curPlayer)
+        .setScale(0.5, 0.5)
+        .setData({
+            alive: true,
+            lives: 5,
+            jump: 0,
+            movedLeft: false,
+            movedRight: false
+        });
+    window[curPlayer].body.setGravityY(300);
+    window[curPlayer].setBounce(0.2);
 }
