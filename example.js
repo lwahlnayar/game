16af9d1961381da371ab011e689e7c300d3d3234
Kyrsten Grima <kyrstengrima@gmail.com>
2018-09-28 07:55:13 +0200

    Update

---
+++ example.js
@@ -0,0 +1,16 @@
+// var config = {
+//     type: Phaser.AUTO,
+//     width: 800,
+//     height: 600,
+//     physics: {
+//         default: "arcade",
+//         arcade: {
+//             gravity: { y: 200 }
+//         }
+//     },
+//     scene: [Example1]
+// };
+//
+// var game = new Phaser.Game(config);
+
+// this is the initial file we tride to follow
